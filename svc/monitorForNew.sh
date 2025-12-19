e2887611862a661cead35ef9cbbfae24623569db
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2025-12-19 20:57:42 +0000

    full path to aws cli

--- svc/monitorForNew.sh
+++ svc/monitorForNew.sh
@@ -24,7 +24,7 @@ ls -1tr 20*.csv | tail -2 | while read i ; do
         yr=${i:0:4}
         mth=${i:5:8}
         targname="s3://mjmm-rawradiodata/raw/event_log_${yr}${mth}"
-        aws s3 cp $i $targname
+        /usr/local/bin/aws s3 cp $i $targname
         cp $i done/
     fi 
 done 
@@ -36,7 +36,7 @@ ls -1tr $latestdir/SMP*.npz | tail -2 | while read i ; do
         grep $i $donelist > /dev/null
         if [ $? == 1 ] ; then 
             targname="s3://mjmm-rawradiodata/tmp/$(basename $i)"
-            aws s3 cp $i $targname
+            /usr/local/bin/aws s3 cp $i $targname
             echo $i >> $donelist
         fi 
     else
